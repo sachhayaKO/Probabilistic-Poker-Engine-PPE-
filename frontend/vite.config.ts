3cd4301d9e9af9b2fa0fe6b87071ae5ef160a016
keth298 <kimethan64@gmail.com>
2026-04-13 23:40:58 -0400

    Frontend Fixes
    Fixing trouble running program

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Probabilistic-Poker-Engine-PPE-/',
+  server: {
+    host: '0.0.0.0',
+  },
 })
